1f63d8f5cc669c2c703ae74fa55ede4d53fcacd9
riho.suzuki <irh-uovospia1222@docomo.ne.jp>
2019-09-27 21:12:00 +0900

    Add database files

--- src/models/Tasklist.java
+++ src/models/Tasklist.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "messages")
+@Table(name = "tasklists")
 public class Tasklist {
     @Id
     @Column(name = "id")
